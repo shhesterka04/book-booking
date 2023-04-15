b1119035fbf77fe712c1376ed2b7936e4da372a5
ArzymKoteyko <artemiy.kotenko@yandex.ru>
2023-04-15 16:52:28 +0300

    working db

--- bot/database/dbapi.py
+++ bot/database/dbapi.py
@@ -1,5 +1,9 @@
+from typing import Tuple, List
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+from .models import *
 
 class DatabaseConnector:
 
@@ -7,30 +11,30 @@ class DatabaseConnector:
         self.engine = create_engine('sqlite:///books.db')
         self.Session = sessionmaker(bind=self.engine)
 
-    def add(self):
+    def add(self, name: str, author: str, pub_year: int) -> tuple[bool, int]:
         session = self.Session()
         session.close() 
 
-    def delete(self):
+    def delete(self, book_id: int) -> bool:
         session = self.Session()
         session.close()
 
-    def list_books(self):
+    def list_books(self) -> List[int]:
         session = self.Session()
         session.close()
 
-    def get_book(self):
+    def get_book(self, name: str, author: str, pub_year: int) -> tuple[bool, int]:
         session = self.Session()
         session.close()
 
-    def borrow(self):
+    def borrow(self, book_id: int) -> tuple[bool, int]:
         session = self.Session()
         session.close()
 
-    def get_borrow(self):
+    def get_borrow(self, name: str, author: str, pub_year: int) -> int:
         session = self.Session()
         session.close()
 
-    def retrieve(self):
+    def retrieve(self) -> None:
         session = self.Session()
         session.close()
--- bot/database/models.py
+++ bot/database/models.py
@@ -1,22 +1,40 @@
-from sqlalchemy import Base, Table, Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship, Mapped
+from datetime import datetime
 
-from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
-class Books(Base):
+class Book(Base):
     __tablename__ = 'books'
-    boock_id = Column(Integer, primary_key=True)
-    title = Column(String())
-    author = Column(String())
-    published = Column(DateTime())
-    date_added = Column(DateTime())
-    date_deleted = Column(DateTime())
 
-class Borrows(Base):
+    boock_id: Mapped[int] = Column(Integer, primary_key=True)
+    title: Mapped[str] = Column(String())
+    author: Mapped[str] = Column(String())
+    published: Mapped[datetime] = Column(DateTime())
+    date_added: Mapped[datetime] = Column(DateTime())
+    date_deleted: Mapped[datetime] = Column(DateTime())
+
+    borrow: Mapped['Borrow'] = relationship('Borrow', back_populates='book')
+
+class Borrow(Base):
     __tablename__ = 'borrows'
-    borrow_id = Column(Integer, primary_key=True)
-    book_id = Column(Integer, ForeignKey('books.boock_id'))
-    date_start = Column(DateTime())
-    date_end = Column(DateTime())
-    user_id = Column(Integer)
+
+    borrow_id: Mapped[int] = Column(Integer, primary_key=True)
+    book_id: Mapped[int] = Column(Integer, ForeignKey('books.boock_id'))
+    date_start: Mapped[datetime] = Column(DateTime())
+    date_end: Mapped[datetime] = Column(DateTime())
+    user_id: Mapped[datetime] = Column(Integer)
+
+    book: Mapped['Book'] = relationship('Book', back_populates='borrow')
+
+
+from sqlalchemy import create_engine
+
+USER = 'Arzym'
+PASSWORD = '201200374art'
+PORT = '5432'
+DBNAME = 'books'
+
+engine = create_engine(f'postgresql://{USER}:{PASSWORD}@localhost:{PORT}/{DBNAME}')
+Base.metadata.create_all(engine)
